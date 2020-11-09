9c86ad86f3f66ea892e60f07feeeffa9c9df2c01
Vincent <mahonv@gmail.com>
2020-11-09 18:40:27 +0100

    Introduce web job

---
+++ MemCheck.NotifierWebjob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemCheck.NotifierWebjob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
