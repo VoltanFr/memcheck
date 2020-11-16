a400aa549db408dfa0b778c75c0fe028b9499764
Vincent <mahonv@gmail.com>
2020-11-16 07:47:00 +0100

    [WebUI] Better admin pages

--- MemCheck.WebUI/Pages/Admin/ManageNavPages.cs
+++ MemCheck.WebUI/Pages/Admin/ManageNavPages.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MemCheck.WebUI.Pages.Admin
@@ -9,10 +6,10 @@ namespace MemCheck.WebUI.Pages.Admin
     public static class ManageNavPages
     {
         public static string Index => "Index";
+        public static string Users => "Users";
+        public static string Languages => "Languages";
 
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
-
-        private static string PageNavClass(ViewContext viewContext, string page)
+        public static string PageNavClass(ViewContext viewContext, string page)
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
                 ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
