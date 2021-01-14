19444aba5d77ce32ea3f6fe83a80c5e645312e5c
Vincent <mahonv@gmail.com>
2021-01-14 19:33:20 +0100

    Compilation fix

--- MemCheck.WebUI/Controllers/AuthoringController.cs
+++ MemCheck.WebUI/Controllers/AuthoringController.cs
@@ -267,7 +267,7 @@ namespace MemCheck.WebUI.Controllers
         public async Task<IActionResult> CardVersions(Guid cardId)
         {
             var userId = await UserServices.UserIdFromContextAsync(HttpContext, userManager);
-            var appResults = await new GetCardVersions(dbContext, this).RunAsync(cardId, userId);
+            var appResults = await new GetCardVersions(dbContext, this).RunAsync(new GetCardVersions.Request(userId, cardId));
             var result = appResults.Select(appResult => new CardVersion(appResult, this));
             return Ok(result);
         }
