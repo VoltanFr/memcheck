4c877f9d08308a3349b0530dc6f2f25555d75322
Vincent <mahonv@gmail.com>
2021-04-26 23:13:57 +0200

    [Application] Delete account, iteration 1: anonymize account

---
+++ MemCheck.Application/Users/DeleteUserAccount.cs
@@ -0,0 +1,57 @@
+﻿using MemCheck.Application.QueryValidation;
+using MemCheck.Database;
+using MemCheck.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MemCheck.Application.Users
+{
+    //Use this when implementing the real user account deletion by himself:                 await signInManager.SignOutAsync();
+    public sealed class DeleteUserAccount
+    {
+        #region Fields
+        private readonly MemCheckDbContext dbContext;
+        private readonly IRoleChecker roleChecker;
+        public const string DeletedUserName = "!DeletedUserName!";
+        public const string DeletedUserEmail = "!DeletedUserEmail!";
+        #endregion
+        public DeleteUserAccount(MemCheckDbContext dbContext, IRoleChecker roleChecker)
+        {
+            this.dbContext = dbContext;
+            this.roleChecker = roleChecker;
+        }
+        public async Task RunAsync(Request request)
+        {
+            await request.CheckValidityAsync(dbContext, roleChecker);
+            var userToDelete = await dbContext.Users.SingleAsync(user => user.Id == request.UserToDeleteId);
+            userToDelete.UserName = DeletedUserName;
+            userToDelete.Email = DeletedUserEmail;
+            userToDelete.EmailConfirmed = false;
+            userToDelete.LockoutEnabled = true;
+            userToDelete.LockoutEnd = DateTime.MaxValue;
+            await dbContext.SaveChangesAsync();
+        }
+        #region Request
+        public sealed record Request(Guid LoggedUserId, Guid UserToDeleteId)
+        {
+            public async Task CheckValidityAsync(MemCheckDbContext dbContext, IRoleChecker roleChecker)
+            {
+                await QueryValidationHelper.CheckUserExistsAsync(dbContext, LoggedUserId);
+                var loggedUser = await dbContext.Users.AsNoTracking().SingleAsync(user => user.Id == LoggedUserId);
+                if (!await roleChecker.UserIsAdminAsync(loggedUser))
+                    //Additional security, to be modified when we really want to support account deletion by user himself
+                    throw new InvalidOperationException("Logged user is not admin");
+
+                await QueryValidationHelper.CheckUserExistsAsync(dbContext, UserToDeleteId);
+                var userToDelete = await dbContext.Users.AsNoTracking().SingleAsync(user => user.Id == UserToDeleteId);
+                if (await roleChecker.UserIsAdminAsync(userToDelete))
+                    //Additional security: forbid deleting an admin account
+                    throw new InvalidOperationException("User to delete is admin");
+            }
+        }
+        #endregion
+    }
+}
---
+++ MemCheck.ApplicationTests/Users/DeleteUserAccountTests.cs
@@ -0,0 +1,90 @@
+﻿using MemCheck.Application.Tests.Helpers;
+using MemCheck.Database;
+using MemCheck.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemCheck.Application.Users
+{
+    [TestClass()]
+    public class DeleteUserAccountTests
+    {
+        [TestMethod()]
+        public async Task UserNotLoggedIn()
+        {
+            var db = DbHelper.GetEmptyTestDB();
+            var userToDelete = await UserHelper.CreateInDbAsync(db);
+
+            using var dbContext = new MemCheckDbContext(db);
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await new DeleteUserAccount(dbContext, new TestRoleChecker()).RunAsync(new DeleteUserAccount.Request(Guid.Empty, userToDelete)));
+        }
+        [TestMethod()]
+        public async Task LoggedUserDoesNotExist()
+        {
+            var db = DbHelper.GetEmptyTestDB();
+            var userToDelete = await UserHelper.CreateInDbAsync(db);
+
+            using var dbContext = new MemCheckDbContext(db);
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await new DeleteUserAccount(dbContext, new TestRoleChecker()).RunAsync(new DeleteUserAccount.Request(Guid.NewGuid(), userToDelete)));
+        }
+        [TestMethod()]
+        public async Task LoggedUserIsNotAdmin()
+        {
+            var db = DbHelper.GetEmptyTestDB();
+            var loggedUser = await UserHelper.CreateInDbAsync(db);
+            var userToDelete = await UserHelper.CreateInDbAsync(db);
+
+            using var dbContext = new MemCheckDbContext(db);
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await new DeleteUserAccount(dbContext, new TestRoleChecker()).RunAsync(new DeleteUserAccount.Request(loggedUser, userToDelete)));
+        }
+        [TestMethod()]
+        public async Task UserToDeleteDoesNotExist()
+        {
+            var db = DbHelper.GetEmptyTestDB();
+            var loggedUser = await UserHelper.CreateInDbAsync(db);
+
+            using var dbContext = new MemCheckDbContext(db);
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await new DeleteUserAccount(dbContext, new TestRoleChecker(loggedUser)).RunAsync(new DeleteUserAccount.Request(loggedUser, Guid.NewGuid())));
+        }
+        [TestMethod()]
+        public async Task UserToDeleteIsAdmin()
+        {
+            var db = DbHelper.GetEmptyTestDB();
+            var loggedUser = await UserHelper.CreateInDbAsync(db);
+            var userToDelete = await UserHelper.CreateInDbAsync(db);
+
+            using var dbContext = new MemCheckDbContext(db);
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await new DeleteUserAccount(dbContext, new TestRoleChecker(loggedUser, userToDelete)).RunAsync(new DeleteUserAccount.Request(loggedUser, userToDelete)));
+        }
+        [TestMethod()]
+        public async Task UserAccountGetsAnonymized()
+        {
+            var db = DbHelper.GetEmptyTestDB();
+            var loggedUser = await UserHelper.CreateInDbAsync(db);
+
+            var userToDeleteName = RandomHelper.String();
+            var userToDeleteEmail = RandomHelper.String();
+            var userToDelete = await UserHelper.CreateInDbAsync(db, userName: userToDeleteName, userEMail: userToDeleteEmail);
+
+            string initialPasswordHash;
+            using (var dbContext = new MemCheckDbContext(db))
+                initialPasswordHash = (await dbContext.Users.SingleAsync(u => u.Id == userToDelete)).PasswordHash;
+
+            using (var dbContext = new MemCheckDbContext(db))
+                await new DeleteUserAccount(dbContext, new TestRoleChecker(loggedUser)).RunAsync(new DeleteUserAccount.Request(loggedUser, userToDelete));
+
+            using (var dbContext = new MemCheckDbContext(db))
+            {
+                var deletedUser = await dbContext.Users.SingleAsync(u => u.Id == userToDelete);
+                Assert.AreEqual(DeleteUserAccount.DeletedUserName, deletedUser.UserName);
+                Assert.AreEqual(DeleteUserAccount.DeletedUserEmail, deletedUser.Email);
+                Assert.IsFalse(deletedUser.EmailConfirmed);
+                Assert.IsTrue(deletedUser.LockoutEnabled);
+                Assert.AreEqual(DateTime.MaxValue, deletedUser.LockoutEnd);
+            }
+        }
+    }
+}
