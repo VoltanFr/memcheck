9614c1da62cdc419a16b0af7b51544e387752ff9
Vincent <mahonv@gmail.com>
2021-06-02 08:31:17 +0200

    Add [DebuggerStepThrough] to ease debugging

--- MemCheck.ApplicationTests/Helpers/TestLocalizer.cs
+++ MemCheck.ApplicationTests/Helpers/TestLocalizer.cs
@@ -1,6 +1,7 @@
 ﻿using MemCheck.Application.QueryValidation;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 
 namespace MemCheck.Application.Tests.Helpers
 {
@@ -13,6 +14,7 @@ namespace MemCheck.Application.Tests.Helpers
         {
             values = ImmutableDictionary.CreateRange(items ?? System.Array.Empty<KeyValuePair<string, string>>());
         }
+        [DebuggerStepThrough]
         public string Get(string resourceName)
         {
             return values.ContainsKey(resourceName) ? values[resourceName] : "";
--- MemCheck.Basics/ObjectExtensions.cs
+++ MemCheck.Basics/ObjectExtensions.cs
@@ -1,7 +1,10 @@
-﻿namespace MemCheck.Basics
+﻿using System.Diagnostics;
+
+namespace MemCheck.Basics
 {
     public static class ObjectExtensions
     {
+        [DebuggerStepThrough]
         public static T[] AsArray<T>(this T t)
         {
             return new[] { t };
