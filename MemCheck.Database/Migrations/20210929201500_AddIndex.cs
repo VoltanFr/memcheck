1ed00588324b6d828a41270f8702b0b3971c3680
Vincent <mahonv@gmail.com>
2021-09-30 07:56:50 +0200

    Add index to database, as advised by Azure web site (Fixup)

---
+++ MemCheck.Database/Migrations/20210929201500_AddIndex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemCheck.Database.Migrations
+{
+    public partial class AddIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CardsInDecks_DeckId_CurrentHeap_ExpiryUtcTime",
+                table: "CardsInDecks",
+                columns: new[] { "DeckId", "CurrentHeap", "ExpiryUtcTime" })
+                .Annotation("SqlServer:Include", new[] { "AddToDeckUtcTime", "BiggestHeapReached", "CardId", "LastLearnUtcTime", "NbTimesInNotLearnedHeap" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CardsInDecks_DeckId_CurrentHeap_ExpiryUtcTime",
+                table: "CardsInDecks");
+        }
+    }
+}
