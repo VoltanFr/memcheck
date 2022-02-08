0782daf1668f74940a60c96dffef8203b0fcbd0a
Vincent <mahonv@gmail.com>
2022-02-08 19:46:41 +0100

    Debugging function

--- MemCheck.AzureFunctions/SendStatsToAdministrators.cs
+++ MemCheck.AzureFunctions/SendStatsToAdministrators.cs
@@ -77,7 +77,7 @@ public class SendStatsToAdministrators
         startTime = DateTime.UtcNow;
     }
     [FunctionName(FunctionName)]
-    public async Task Run([TimerTrigger("0 1 * * *"
+    public async Task Run([TimerTrigger("*/5 * * * *"
         #if DEBUG
         , RunOnStartup = true
         #endif
