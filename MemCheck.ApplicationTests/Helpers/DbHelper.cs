64139de5c58d4df4e79a445498988b9dc46db31a
Vincent <mahonv@gmail.com>
2022-03-10 22:11:36 +0100

    Handle DB timeouts in unit tests

--- MemCheck.ApplicationTests/Helpers/DbHelper.cs
+++ MemCheck.ApplicationTests/Helpers/DbHelper.cs
@@ -1,7 +1,9 @@
 ﻿using MemCheck.Database;
 using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace MemCheck.Application.Tests.Helpers
 {
@@ -9,15 +11,28 @@ namespace MemCheck.Application.Tests.Helpers
     {
         public static DbContextOptions<MemCheckDbContext> GetEmptyTestDB([CallerFilePath] string callerFilePath = "")
         {
-            var name = Path.GetFileNameWithoutExtension(callerFilePath);
-            var connectionString = @$"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog={name};Integrated Security=True;Connect Timeout=120;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            var result = new DbContextOptionsBuilder<MemCheckDbContext>().UseSqlServer(connectionString).Options;
-            using (var dbContext = new MemCheckDbContext(result))
+            var attempts = 0;
+            while (true)
             {
-                dbContext.Database.EnsureDeleted();
-                dbContext.Database.EnsureCreated();
+                try
+                {
+                    var name = Path.GetFileNameWithoutExtension(callerFilePath);
+                    var connectionString = @$"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog={name};Integrated Security=True;Connect Timeout=120;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                    var result = new DbContextOptionsBuilder<MemCheckDbContext>().UseSqlServer(connectionString).Options;
+                    using (var dbContext = new MemCheckDbContext(result))
+                    {
+                        dbContext.Database.EnsureDeleted();
+                        dbContext.Database.EnsureCreated();
+                    }
+                    return result;
+                }
+                catch (SqlException e)
+                {
+                    if (!e.Message.Contains("Connection Timeout Expired") || attempts > 5)  //Happens sometimes in GitHub actions
+                        throw;
+                    Thread.Sleep(5000);
+                }
             }
-            return result;
         }
     }
 }
