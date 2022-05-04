69d0027ffc015065ae4ff465596efe157106585d
Vincent <mahonv@gmail.com>
2022-05-04 23:59:35 +0200

    Deploy attempt 2

--- MemCheck.AzureFunctions/StartUp.cs
+++ MemCheck.AzureFunctions/StartUp.cs
@@ -1,7 +1,6 @@
 ﻿using MemCheck.Application.Users;
 using MemCheck.Database;
 using MemCheck.Domain;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,36 +9,16 @@ using System;
 [assembly: FunctionsStartup(typeof(MemCheck.AzureFunctions.Startup))]
 
 namespace MemCheck.AzureFunctions;
+
 public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
         builder.Services.AddDbContext<MemCheckDbContext>(options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, Environment.GetEnvironmentVariable("MemCheckDbConnectionString")));
 
-        builder.Services.AddIdentityCore<MemCheckUser>(opt =>
-        {
-            opt.Password.RequireDigit = false;
-            opt.Password.RequireLowercase = false;
-            opt.Password.RequireNonAlphanumeric = false;
-            opt.Password.RequireUppercase = false;
-        })
-            .AddSignInManager()
-            .AddEntityFrameworkStores<MemCheckDbContext>()
+        builder.Services.AddIdentityCore<MemCheckUser>(opt => { opt.SignIn.RequireConfirmedAccount = true; opt.User.RequireUniqueEmail = false; })
+            .AddRoles<MemCheckUserRole>()
             .AddUserManager<MemCheckUserManager>()
-            .AddDefaultTokenProviders();
-
-
-        //Microsoft.AspNetCore.Identity.IdentityBuilder identityBuilder = builder.Services
-        //            .AddIdentity<MemCheckUser, MemCheckUserRole>(
-        //            options =>
-        //                {
-        //                    options.SignIn.RequireConfirmedAccount = true;
-        //                    options.User.RequireUniqueEmail = false;
-        //                });
-
-
-        //identityBuilder
-        //    .AddEntityFrameworkStores<MemCheckDbContext>()
-        //    .AddUserManager<MemCheckUserManager>();
+            .AddEntityFrameworkStores<MemCheckDbContext>();
     }
 }
