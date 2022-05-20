049a837cd09478a3e1e4344a53e01be9b9f20491
Vincent <mahonv@gmail.com>
2022-05-20 13:13:11 +0200

    Improve deck creation page for warning about not creating multiple decks

--- MemCheck.WebUI/wwwroot/js/Decks/Create.js
+++ MemCheck.WebUI/wwwroot/js/Decks/Create.js
@@ -9,11 +9,14 @@ const createDeckApp = Vue.createApp({
             description: '',
             heapingAlgorithm: '',   // DecksController.HeapingAlgorithmViewModel
             mountFinished: false,
+            userDeckNames: [],  // strings
         };
     },
     async mounted() {
         try {
-            await this.getHeapingAlgorithms();
+            const task1 = await this.getHeapingAlgorithms();
+            const task2 = await this.getUserDeckNames();
+            await Promise.all([task1, task2]);
         }
         finally {
             this.mountFinished = true;
@@ -29,6 +32,17 @@ const createDeckApp = Vue.createApp({
                     tellAxiosError(error);
                 });
         },
+        async getUserDeckNames() {
+            await axios.get('/Decks/GetUserDecks/')
+                .then(result => {
+                    for (let i = 0; i < result.data.length; i++) {
+                        this.userDeckNames.push(result.data[i].description);
+                    }
+                })
+                .catch(error => {
+                    tellAxiosError(error);
+                });
+        },
         async create() {
             const newDeck = { description: this.description, heapingAlgorithmId: this.heapingAlgorithm.id };
             await axios.post('/Decks/Create/', newDeck)
@@ -38,6 +52,9 @@ const createDeckApp = Vue.createApp({
                 .catch(error => {
                     tellAxiosError(error);
                 });
+        },
+        userHasDecks() {
+            return this.userDeckNames.length > 0;
         },
     },
 });
