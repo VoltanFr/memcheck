51d75816cfa51c6d737cb5d1ffd150a1bb57f153
Vincent <mahonv@gmail.com>
2022-05-28 10:32:06 +0200

    CommandLineDbClient: add RateAllPublicCards

--- MemCheck.CommandLineDbClient/Engine.cs
+++ MemCheck.CommandLineDbClient/Engine.cs
@@ -1,4 +1,4 @@
-﻿using MemCheck.CommandLineDbClient.Geography;
+﻿using MemCheck.CommandLineDbClient.Ratings;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -17,7 +17,7 @@ internal sealed class Engine : IHostedService
     #region Private method
     private ICmdLinePlugin GetPlugin()
     {
-        return new AddRefToUsStates(serviceProvider);
+        return new RateAllPublicCards(serviceProvider);
     }
     #endregion
     public Engine(ILogger<Engine> logger, IServiceProvider serviceProvider)
---
+++ MemCheck.CommandLineDbClient/Ratings/RateAllPublicCards.cs
@@ -0,0 +1,96 @@
+﻿using MemCheck.Application.Ratings;
+using MemCheck.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemCheck.CommandLineDbClient.Ratings;
+
+internal sealed class RateAllPublicCards : ICmdLinePlugin
+{
+    #region Fields
+    private readonly ILogger<RateAllBotCards> logger;
+    private readonly MemCheckDbContext dbContext;
+    private readonly Guid userId;
+    private readonly Guid englishVocabularyTagId;
+    private readonly ImmutableDictionary<Guid, int> userRatings;
+    #endregion
+    #region Private record CardFromDb
+    private record CardFromDb(Guid Id, string FrontSide, string AdditionalInfo, string References, ImmutableHashSet<Guid> TagIds);
+    #endregion
+    #region Private methods
+    private async Task RateEnglishCardsAsync()
+    {
+        var cards = dbContext.Cards
+            .AsNoTracking()
+            .Where(card => !card.UsersWithView.Any() && card.TagsInCards.Any(tag => tag.TagId == englishVocabularyTagId))
+            .Select(card => new { card.Id, card.FrontSide, card.AdditionalInfo })
+            .ToImmutableArray();
+
+        logger.LogInformation($"{cards.Length} English cards");
+        var rater = new SetCardRating(dbContext.AsCallContext());
+        var i = 1;
+
+        foreach (var card in cards)
+        {
+            var ratingToSetForCard = card.AdditionalInfo.Length > 0 ? 5 : 3;
+            if (!userRatings.TryGetValue(card.Id, out var existingRating) || ratingToSetForCard != existingRating)
+            {
+                logger.LogInformation($"[English {i} of {cards.Length}]: setting rating {ratingToSetForCard} on {card.Id} ({card.FrontSide})");
+                var request = new SetCardRating.Request(userId, card.Id, ratingToSetForCard);
+                await rater.RunAsync(request);
+            }
+            i++;
+        }
+
+        logger.LogInformation($"English rating finished");
+    }
+    private async Task RateNonEnglishCardsAsync()
+    {
+        var cards = dbContext.Cards
+            .AsNoTracking()
+            .Where(card => !card.UsersWithView.Any() && !card.TagsInCards.Any(tag => tag.TagId == englishVocabularyTagId))
+            .Select(card => new { card.Id, card.FrontSide, card.AdditionalInfo, card.References })
+            .ToImmutableArray();
+
+        logger.LogInformation($"{cards.Length} non-English cards");
+        var rater = new SetCardRating(dbContext.AsCallContext());
+        var i = 1;
+
+        foreach (var card in cards)
+        {
+            var ratingToSetForCard = card.AdditionalInfo.Length == 0 ? 3 : (card.References.Length == 0 ? 4 : 5);
+            if (!userRatings.TryGetValue(card.Id, out var existingRating) || ratingToSetForCard != existingRating)
+            {
+                logger.LogInformation($"[Non-English {i} of {cards.Length}]: setting rating {ratingToSetForCard} on {card.Id} ({card.FrontSide})");
+                var request = new SetCardRating.Request(userId, card.Id, ratingToSetForCard);
+                await rater.RunAsync(request);
+            }
+            i++;
+        }
+
+        logger.LogInformation($"Other than English rating finished");
+    }
+    #endregion
+    public RateAllPublicCards(IServiceProvider serviceProvider)
+    {
+        dbContext = serviceProvider.GetRequiredService<MemCheckDbContext>();
+        logger = serviceProvider.GetRequiredService<ILogger<RateAllBotCards>>();
+        userId = dbContext.Users.Single(u => u.UserName == "VoltanBot").Id;
+        englishVocabularyTagId = dbContext.Tags.Single(tag => tag.Name == "Vocabulaire anglais").Id;
+        userRatings = dbContext.UserCardRatings.AsNoTracking().Where(rating => rating.UserId == userId).ToImmutableDictionary(rating => rating.CardId, rating => rating.Rating);
+    }
+    public void DescribeForOpportunityToCancel()
+    {
+        logger.LogInformation($"Will rate all public cards");
+    }
+    public async Task RunAsync()
+    {
+        await RateEnglishCardsAsync();
+        await RateNonEnglishCardsAsync();
+    }
+}
