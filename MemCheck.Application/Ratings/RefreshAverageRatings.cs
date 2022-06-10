f33e045128b42402d6bbcd76424818f47486d439
Vincent <mahonv@gmail.com>
2022-06-10 07:42:39 +0200

    New Application service: RefreshAverageRatings

---
+++ MemCheck.Application/Ratings/RefreshAverageRatings.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemCheck.Application.Ratings;
+
+public sealed class RefreshAverageRatings : RequestRunner<RefreshAverageRatings.Request, RefreshAverageRatings.Result>
+{
+    public RefreshAverageRatings(CallContext callContext) : base(callContext)
+    {
+    }
+    protected override async Task<ResultWithMetrologyProperties<Result>> DoRunAsync(Request request)
+    {
+        var cardsFromDb = (await DbContext.Cards.ToDictionaryAsync(card => card.Id, card => card)).ToImmutableDictionary();
+
+        var averageRatingsFromRealValues = (await DbContext.UserCardRatings.AsNoTracking()
+            .GroupBy(userCardRating => userCardRating.CardId)
+            .Select(group => new { CardId = group.Key, AverageRating = group.Average(g => g.Rating) })
+            .ToDictionaryAsync(averageCardRating => averageCardRating.CardId, averageCardRating => averageCardRating.AverageRating))
+            .ToImmutableDictionary();
+
+        var changedAverageRatingCount = 0;
+        foreach (var cardId in cardsFromDb.Keys)
+        {
+            var newAverage = averageRatingsFromRealValues.ContainsKey(cardId) ? averageRatingsFromRealValues[cardId] : 0;
+
+            if (cardsFromDb[cardId].AverageRating != newAverage)
+            {
+                cardsFromDb[cardId].AverageRating = newAverage;
+                changedAverageRatingCount++;
+            }
+        }
+
+        await DbContext.SaveChangesAsync();
+
+        var result = new Result(cardsFromDb.Count, changedAverageRatingCount);
+
+        return new ResultWithMetrologyProperties<Result>(result,
+            IntMetric("TotalCardCountInDb", result.TotalCardCountInDb),
+            IntMetric("ChangedAverageRatingCount", result.ChangedAverageRatingCount));
+    }
+    #region Request & Result
+    public sealed record Request : IRequest
+    {
+        public async Task CheckValidityAsync(CallContext callContext)
+        {
+            await Task.CompletedTask;
+        }
+    }
+    public sealed record Result(int TotalCardCountInDb, int ChangedAverageRatingCount);
+    #endregion
+}
---
+++ MemCheck.ApplicationTests/Ratings/RefreshAverageRatingsTests.cs
@@ -0,0 +1,144 @@
+﻿using MemCheck.Application.Helpers;
+using MemCheck.Database;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemCheck.Application.Ratings;
+
+[TestClass()]
+public class RefreshAverageRatingsTests
+{
+    [TestMethod()]
+    public async Task NoCard()
+    {
+        var db = DbHelper.GetEmptyTestDB();
+
+        using var dbContext = new MemCheckDbContext(db);
+        var result = await new RefreshAverageRatings(dbContext.AsCallContext()).RunAsync(new RefreshAverageRatings.Request());
+        Assert.AreEqual(0, result.TotalCardCountInDb);
+        Assert.AreEqual(0, result.ChangedAverageRatingCount);
+    }
+    [TestMethod()]
+    public async Task OneCardWithoutRating()
+    {
+        var db = DbHelper.GetEmptyTestDB();
+        var userId = await UserHelper.CreateInDbAsync(db);
+        await CardHelper.CreateAsync(db, userId);
+
+        using var dbContext = new MemCheckDbContext(db);
+        var result = await new RefreshAverageRatings(dbContext.AsCallContext()).RunAsync(new RefreshAverageRatings.Request());
+        Assert.AreEqual(1, result.TotalCardCountInDb);
+        Assert.AreEqual(0, result.ChangedAverageRatingCount);
+    }
+    [TestMethod()]
+    public async Task OneCardWithAverageRatingUpToDate()
+    {
+        var db = DbHelper.GetEmptyTestDB();
+        var cardCreatorId = await UserHelper.CreateInDbAsync(db);
+        var cardId = await CardHelper.CreateIdAsync(db, cardCreatorId);
+
+        var ratingByCreator = RandomHelper.Rating();
+        await RatingHelper.RecordForUserAsync(db, cardCreatorId, cardId, ratingByCreator);
+
+        var otherUserId = await UserHelper.CreateInDbAsync(db);
+        var ratingByOtherUser = RandomHelper.Rating(ratingByCreator);
+        await RatingHelper.RecordForUserAsync(db, otherUserId, cardId, ratingByOtherUser);
+
+        using (var dbContext = new MemCheckDbContext(db))
+        {
+            var result = await new RefreshAverageRatings(dbContext.AsCallContext()).RunAsync(new RefreshAverageRatings.Request());
+            Assert.AreEqual(1, result.TotalCardCountInDb);
+            Assert.AreEqual(0, result.ChangedAverageRatingCount);
+        }
+
+        using (var dbContext = new MemCheckDbContext(db))
+            Assert.AreEqual((double)(ratingByCreator + ratingByOtherUser) / 2, dbContext.Cards.Single().AverageRating);
+    }
+    [TestMethod()]
+    public async Task OneCardWithAverageRatingNotUpToDate()
+    {
+        var db = DbHelper.GetEmptyTestDB();
+        var cardCreatorId = await UserHelper.CreateInDbAsync(db);
+        var cardId = await CardHelper.CreateIdAsync(db, cardCreatorId);
+
+        var ratingByCreator = RandomHelper.Rating();
+        await RatingHelper.RecordForUserAsync(db, cardCreatorId, cardId, ratingByCreator);
+
+        var otherUserId = await UserHelper.CreateInDbAsync(db);
+        var ratingByOtherUser = RandomHelper.Rating(ratingByCreator);
+        using (var dbContext = new MemCheckDbContext(db))
+        {
+            var userCardRating = new Domain.UserCardRating
+            {
+                UserId = otherUserId,
+                CardId = cardId,
+                Rating = ratingByOtherUser
+            };
+            dbContext.UserCardRatings.Add(userCardRating);
+            dbContext.SaveChanges();
+        };
+
+        using (var dbContext = new MemCheckDbContext(db))
+        {
+            var result = await new RefreshAverageRatings(dbContext.AsCallContext()).RunAsync(new RefreshAverageRatings.Request());
+            Assert.AreEqual(1, result.TotalCardCountInDb);
+            Assert.AreEqual(1, result.ChangedAverageRatingCount);
+        }
+
+        using (var dbContext = new MemCheckDbContext(db))
+            Assert.AreEqual((double)(ratingByCreator + ratingByOtherUser) / 2, dbContext.Cards.Single().AverageRating);
+    }
+    [TestMethod()]
+    public async Task ComplexCase()
+    {
+        var db = DbHelper.GetEmptyTestDB();
+        var cardCreatorId = await UserHelper.CreateInDbAsync(db);
+        var ratingUserIds = Enumerable.Range(0, 20).Select(i => UserHelper.CreateInDbAsync(db).Result).ToImmutableArray();
+
+        var cardRatings = new Dictionary<Guid, double>();
+
+        for (var cardIndex = 0; cardIndex < 50; cardIndex++)
+        {
+            var cardId = await CardHelper.CreateIdAsync(db, cardCreatorId);
+            var ratings = new List<int>();
+            for (var ratingIndex = 0; ratingIndex < RandomHelper.Int(0, 19); ratingIndex++)
+            {
+                var rating = RandomHelper.Rating();
+                if (ratingIndex != 0 && RandomHelper.Bool())
+                {
+                    var ratingByCreator = RandomHelper.Rating();
+                    await RatingHelper.RecordForUserAsync(db, ratingUserIds[ratingIndex], cardId, rating);
+                }
+                else
+                {
+                    using (var dbContext = new MemCheckDbContext(db))
+                    {
+                        var userCardRating = new Domain.UserCardRating { UserId = ratingUserIds[ratingIndex], CardId = cardId, Rating = rating };
+                        dbContext.UserCardRatings.Add(userCardRating);
+                        dbContext.SaveChanges();
+                    };
+                }
+                ratings.Add(rating);
+            }
+            cardRatings.Add(cardId, ratings.Any() ? ratings.Average() : 0);
+        }
+
+        using (var dbContext = new MemCheckDbContext(db))
+        {
+            var result = await new RefreshAverageRatings(dbContext.AsCallContext()).RunAsync(new RefreshAverageRatings.Request());
+            Assert.AreEqual(50, result.TotalCardCountInDb);
+            Assert.AreEqual(cardRatings.Count(cardRating => cardRating.Value != 0), result.ChangedAverageRatingCount);
+        }
+
+        using (var dbContext = new MemCheckDbContext(db))
+            foreach (var cardId in cardRatings.Keys)
+            {
+                var cardFromDb = dbContext.Cards.Single(card => card.Id == cardId);
+                Assert.AreEqual(cardRatings[cardId], cardFromDb.AverageRating);
+            }
+    }
+}
