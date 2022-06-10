ea6b8e5e79cbcd16ba1087238a49230add34e976
Vincent <mahonv@gmail.com>
2022-06-10 08:09:07 +0200

    New Azure func : RefreshAverageRatings
    Closes #115

--- MemCheck.AzureFunctions/Constants.cs
+++ MemCheck.AzureFunctions/Constants.cs
@@ -3,9 +3,10 @@
 internal static class Constants
 {
     public const string Cron_RateAllPublicCards = "0 0 1 * * *";
-    public const string Cron_SendStatsToAdministrators = "0 0 2 * * *";
+    public const string Cron_SendStatsToAdministrators = "0 30 1 * * *";
+    public const string Cron_RefreshAverageRatings = "0 0 2 * * *";
+    public const string Cron_MakeWikipediaLinksDesktop = "0 30 2 * * *";
     public const string Cron_UpdateTagStats = "0 0 3 * * *";
-    public const string Cron_MakeWikipediaLinksDesktop = "0 0 4 * * *";
     public const string Cron_SendNotifications = "0 0 5 * * *";
     //public const string CronEach5Min = "0 */5 * * * *";
 }
---
+++ MemCheck.AzureFunctions/RefreshAverageRatings.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using MemCheck.Application.Users;
+using MemCheck.Database;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace MemCheck.AzureFunctions;
+
+public sealed class RefreshAverageRatings : AbstractMemCheckAzureFunction
+{
+    public RefreshAverageRatings(TelemetryConfiguration telemetryConfiguration, MemCheckDbContext memCheckDbContext, MemCheckUserManager userManager, ILogger<UpdateTagStats> logger)
+        : base(telemetryConfiguration, memCheckDbContext, userManager, logger)
+    {
+    }
+    [FunctionName(nameof(RefreshAverageRatings))]
+    public async Task Run([TimerTrigger(Constants.Cron_RefreshAverageRatings)] TimerInfo timer, ExecutionContext context)
+    {
+        await RunAsync(timer, context);
+    }
+    protected override async Task<string> RunAndCreateReportMailMainPartAsync()
+    {
+        var refresher = new Application.Ratings.RefreshAverageRatings(NewCallContext());
+        var result = await refresher.RunAsync(new Application.Ratings.RefreshAverageRatings.Request());
+
+        var reportMailMainPart = new StringBuilder()
+            .Append("<p>")
+            .Append(CultureInfo.InvariantCulture, $"<li>Considered {result.TotalCardCountInDb} cards</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>Changed the average ratings for {result.ChangedAverageRatingCount} cards</li>")
+            .Append("</p>");
+
+        return reportMailMainPart.ToString();
+    }
+}
