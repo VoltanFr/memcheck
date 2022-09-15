8c0c932106c85878b1e5221c80646f87fdd40717
Vincent <mahonv@gmail.com>
2022-09-16 01:03:24 +0200

    New Azure function: RefreshImageUsages

--- MemCheck.AzureFunctions/Constants.cs
+++ MemCheck.AzureFunctions/Constants.cs
@@ -7,6 +7,7 @@ internal static class Constants
     public const string Cron_RefreshAverageRatings = "0 0 2 * * *";
     public const string Cron_MakeWikipediaLinksDesktop = "0 30 2 * * *";
     public const string Cron_UpdateTagStats = "0 0 3 * * *";
-    public const string Cron_SendNotifications = "0 0 5 * * *";
+    public const string Cron_RefreshImageUsages = "0 30 3 * * *";
+    public const string Cron_SendNotifications = "0 0 4 * * *";
     //public const string CronEach5Min = "0 */5 * * * *";
 }
---
+++ MemCheck.AzureFunctions/RefreshImageUsages.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using MemCheck.Application.Images;
+using MemCheck.Application.Users;
+using MemCheck.Database;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace MemCheck.AzureFunctions;
+
+public sealed class RefreshImageUsages : AbstractMemCheckAzureFunction
+{
+    public RefreshImageUsages(TelemetryConfiguration telemetryConfiguration, MemCheckDbContext memCheckDbContext, MemCheckUserManager userManager, ILogger<UpdateTagStats> logger)
+        : base(telemetryConfiguration, memCheckDbContext, userManager, logger)
+    {
+    }
+    [FunctionName(nameof(RefreshImageUsages))]
+    public async Task Run([TimerTrigger(Constants.Cron_RefreshImageUsages)] TimerInfo timer, ExecutionContext context)
+    {
+        await RunAsync(timer, context);
+    }
+    protected override async Task<string> RunAndCreateReportMailMainPartAsync()
+    {
+        var updater = new RefreshImagesInCards(NewCallContext());
+        var result = await updater.RunAsync(new RefreshImagesInCards.Request());
+
+        var reportMailMainPart = new StringBuilder()
+            .Append(CultureInfo.InvariantCulture, $"<h1>Refresh image usages table</h1>")
+            .Append("<p><ul>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.TotalImageCount} images in DB</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.TotalCardCount} cards in DB</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.ImagesInCardsCountOnStart} image usages on start</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.ImagesInCardsCountOnEnd} image usages on end</li>")
+            .Append("</ul></p>");
+
+        return reportMailMainPart.ToString();
+    }
+}
