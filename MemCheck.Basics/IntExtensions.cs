725b5cabbb773a2e0fc3c6b8d2db370b22266618
Vincent <mahonv@gmail.com>
2023-01-22 09:25:33 +0100

    int.Times extension method

--- MemCheck.Basics/IntExtensions.cs
+++ MemCheck.Basics/IntExtensions.cs
@@ -7,6 +7,22 @@ namespace MemCheck.Basics;
 
 public static class IntExtensions
 {
+    public static void Times(this int count, Action action)
+    {
+        if (count < 0)
+            throw new ArgumentOutOfRangeException(nameof(count), $"Can't do {count} times");
+        for (var i = 0; i < count; i++)
+            action();
+    }
+    public static ImmutableArray<TResult> Times<TResult>(this int count, Func<TResult> func)
+    {
+        if (count < 0)
+            throw new ArgumentOutOfRangeException(nameof(count), $"Can't do {count} times");
+        var result = new List<TResult>();
+        for (var i = 0; i < count; i++)
+            result.Add(func());
+        return result.ToImmutableArray();
+    }
     public static async Task TimesAsync(this int count, Func<Task> action)
     {
         if (count < 0)
