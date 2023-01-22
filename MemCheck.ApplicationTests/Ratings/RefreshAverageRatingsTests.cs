6e0ad7a5d5193f72e463bfd3e4bc13ee0d305d00
Vincent <mahonv@gmail.com>
2023-01-22 11:06:51 +0100

    Fix a unit test

--- MemCheck.ApplicationTests/Ratings/RefreshAverageRatingsTests.cs
+++ MemCheck.ApplicationTests/Ratings/RefreshAverageRatingsTests.cs
@@ -1,9 +1,9 @@
 ﻿using MemCheck.Application.Helpers;
+using MemCheck.Basics;
 using MemCheck.Database;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -95,43 +95,56 @@ public class RefreshAverageRatingsTests
     [TestMethod()]
     public async Task ComplexCase()
     {
+        // One user who creates cards, named cardCreator
+        // Some users who give ratings to cards: ratingUserIds
+        // cardCreator creates some cards
+        // For each card, we record some ratings without refreshing the cards's average rating, and with a probability of 0.5 we set a rating by cardCreator WITH refreshing the card's average rating
+        // Then we launch a refresh and check the results
+
         var db = DbHelper.GetEmptyTestDB();
         var cardCreatorId = await UserHelper.CreateInDbAsync(db);
-        var ratingUserIds = Enumerable.Range(0, 20).Select(i => UserHelper.CreateInDbAsync(db).Result).ToImmutableArray();
+        var ratingUserIds = await RandomHelper.Int(20, 30).TimesAsync(() => UserHelper.CreateInDbAsync(db));
 
         var cardRatings = new Dictionary<Guid, double>();
+        var cardCount = RandomHelper.Int(50, 100);
+        var countOfCardsWithCorrectAverageRating = 0;
 
-        for (var cardIndex = 0; cardIndex < 50; cardIndex++)
-        {
-            var cardId = await CardHelper.CreateIdAsync(db, cardCreatorId);
-            var ratings = new List<int>();
-            for (var ratingIndex = 0; ratingIndex < RandomHelper.Int(0, 19); ratingIndex++)
-            {
-                var rating = RandomHelper.Rating();
-                if (ratingIndex != 0 && RandomHelper.Bool())
+        await cardCount.TimesAsync(async () =>
                 {
-                    var ratingByCreator = RandomHelper.Rating();
-                    await RatingHelper.RecordForUserAsync(db, ratingUserIds[ratingIndex], cardId, rating);
-                }
-                else
-                {
+                    var cardId = await CardHelper.CreateIdAsync(db, cardCreatorId);
+                    var ratings = new List<int>();
+
                     using (var dbContext = new MemCheckDbContext(db))
                     {
-                        var userCardRating = new Domain.UserCardRating { UserId = ratingUserIds[ratingIndex], CardId = cardId, Rating = rating };
-                        dbContext.UserCardRatings.Add(userCardRating);
+                        // Add some ratings without refreshing the card's average rating
+                        for (var ratingIndex = 0; ratingIndex < RandomHelper.Int(1, ratingUserIds.Length + 1); ratingIndex++)
+                        {
+                            var rating = RandomHelper.Rating();
+                            dbContext.UserCardRatings.Add(new Domain.UserCardRating { UserId = ratingUserIds[ratingIndex], CardId = cardId, Rating = rating });
+                            ratings.Add(rating);
+                        }
                         dbContext.SaveChanges();
-                    };
-                }
-                ratings.Add(rating);
-            }
-            cardRatings.Add(cardId, ratings.Any() ? ratings.Average() : 0);
-        }
+                    }
+
+                    if (RandomHelper.Bool())
+                    {
+                        // Add a rating by cardCreator and refresh the card's average rating
+                        var creatorRating = RandomHelper.Rating();
+                        await RatingHelper.RecordForUserAsync(db, cardCreatorId, cardId, creatorRating); // Updates the average rating
+                        ratings.Add(creatorRating);
+                        countOfCardsWithCorrectAverageRating++;
+                    }
+
+                    cardRatings.Add(cardId, ratings.Average());
+                });
+
+        await CardHelper.CreateIdAsync(db, cardCreatorId); // Ensure there is at least a card without rating
 
         using (var dbContext = new MemCheckDbContext(db))
         {
             var result = await new RefreshAverageRatings(dbContext.AsCallContext()).RunAsync(new RefreshAverageRatings.Request());
-            Assert.AreEqual(50, result.TotalCardCountInDb);
-            Assert.AreEqual(cardRatings.Count(cardRating => cardRating.Value != 0), result.ChangedAverageRatingCount);
+            Assert.AreEqual(cardCount + 1, result.TotalCardCountInDb);
+            Assert.AreEqual(cardRatings.Count - countOfCardsWithCorrectAverageRating, result.ChangedAverageRatingCount);
         }
 
         using (var dbContext = new MemCheckDbContext(db))
