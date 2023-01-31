042efb6816ba01280a693cf4231315f8dad16bdf
Vincent <mahonv@gmail.com>
2023-01-31 08:02:13 +0100

    Reactivate all Azure functions

--- MemCheck.AzureFunctions/RefreshImageUsages.cs
+++ MemCheck.AzureFunctions/RefreshImageUsages.cs
@@ -1,49 +1,50 @@
-﻿//using System.Globalization;
-//using System.Text;
-//using System.Threading.Tasks;
-//using MemCheck.Application.Images;
-//using MemCheck.Application.Users;
-//using MemCheck.Database;
-//using Microsoft.ApplicationInsights.Extensibility;
-//using Microsoft.Azure.Functions.Worker;
+﻿using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using MemCheck.Application.Images;
+using MemCheck.Application.Users;
+using MemCheck.Database;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
-//namespace MemCheck.AzureFunctions;
+namespace MemCheck.AzureFunctions;
 
-//public sealed class RefreshImageUsages : AbstractMemCheckAzureFunction
-//{
-//    public RefreshImageUsages(TelemetryConfiguration telemetryConfiguration, MemCheckDbContext memCheckDbContext, MemCheckUserManager userManager)
-//        : base(telemetryConfiguration, memCheckDbContext, userManager)
-//    {
-//    }
-//    [Function(nameof(RefreshImageUsages))]
-//    public async Task Run([TimerTrigger(Constants.Cron_RefreshImageUsages)] TimerInfo timer, FunctionContext context)
-//    {
-//        await RunAsync(timer, context);
-//    }
-//    protected override async Task<RunResult> RunAndCreateReportMailMainPartAsync(string defaultMailSubject)
-//    {
-//        var updater = new RefreshImagesInCards(NewCallContext());
-//        var result = await updater.RunAsync(new RefreshImagesInCards.Request());
+public sealed class RefreshImageUsages : AbstractMemCheckAzureFunction
+{
+    public RefreshImageUsages(TelemetryConfiguration telemetryConfiguration, MemCheckDbContext memCheckDbContext, MemCheckUserManager userManager, ILogger<RefreshImageUsages> logger)
+        : base(telemetryConfiguration, memCheckDbContext, userManager, logger)
+    {
+    }
+    [Function(nameof(RefreshImageUsages))]
+    public async Task Run([TimerTrigger(Constants.Cron_RefreshImageUsages)] TimerInfo timer, FunctionContext context)
+    {
+        await RunAsync(timer, context);
+    }
+    protected override async Task<RunResult> RunAndCreateReportMailMainPartAsync(string defaultMailSubject)
+    {
+        var updater = new RefreshImagesInCards(NewCallContext());
+        var result = await updater.RunAsync(new RefreshImagesInCards.Request());
 
-//        var reportMailMainPart = new StringBuilder()
-//            .Append(CultureInfo.InvariantCulture, $"<h1>Refresh image usages table</h1>")
-//            .Append("<p><ul>")
-//            .Append(CultureInfo.InvariantCulture, $"<li>{result.TotalImageCount} images in DB</li>")
-//            .Append(CultureInfo.InvariantCulture, $"<li>{result.TotalCardCount} cards in DB</li>")
-//            .Append(CultureInfo.InvariantCulture, $"<li>{result.ImagesInCardsCountOnStart} image usages on start</li>")
-//            .Append(CultureInfo.InvariantCulture, $"<li>{result.ImagesInCardsCountOnEnd} image usages on end</li>")
-//            .Append(CultureInfo.InvariantCulture, $"<li>{result.ChangeCount} total changes</li>")
-//            .Append("</ul></p>");
+        var reportMailMainPart = new StringBuilder()
+            .Append(CultureInfo.InvariantCulture, $"<h1>Refresh image usages table</h1>")
+            .Append("<p><ul>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.TotalImageCount} images in DB</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.TotalCardCount} cards in DB</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.ImagesInCardsCountOnStart} image usages on start</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.ImagesInCardsCountOnEnd} image usages on end</li>")
+            .Append(CultureInfo.InvariantCulture, $"<li>{result.ChangeCount} total changes</li>")
+            .Append("</ul></p>");
 
-//        var changeInfo = result.ChangeCount switch
-//        {
-//            0 => "no change",
-//            1 => "1 change",
-//            _ => $"{result.ChangeCount} changes",
-//        };
+        var changeInfo = result.ChangeCount switch
+        {
+            0 => "no change",
+            1 => "1 change",
+            _ => $"{result.ChangeCount} changes",
+        };
 
-//        var mailSubject = $"{defaultMailSubject} ({changeInfo})";
+        var mailSubject = $"{defaultMailSubject} ({changeInfo})";
 
-//        return new RunResult(mailSubject, reportMailMainPart);
-//    }
-//}
+        return new RunResult(mailSubject, reportMailMainPart);
+    }
+}
--- MemCheck.AzureFunctions/UpdateTagStats.cs
+++ MemCheck.AzureFunctions/UpdateTagStats.cs
@@ -1,79 +1,80 @@
-﻿//using System.Globalization;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using MemCheck.Application.Tags;
-//using MemCheck.Application.Users;
-//using MemCheck.Database;
-//using Microsoft.ApplicationInsights.Extensibility;
-//using Microsoft.Azure.Functions.Worker;
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemCheck.Application.Tags;
+using MemCheck.Application.Users;
+using MemCheck.Database;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
-//namespace MemCheck.AzureFunctions;
+namespace MemCheck.AzureFunctions;
 
-//public sealed class UpdateTagStats : AbstractMemCheckAzureFunction
-//{
-//    public UpdateTagStats(TelemetryConfiguration telemetryConfiguration, MemCheckDbContext memCheckDbContext, MemCheckUserManager userManager)
-//        : base(telemetryConfiguration, memCheckDbContext, userManager)
-//    {
-//    }
-//    [Function(nameof(UpdateTagStats))]
-//    public async Task Run([TimerTrigger(Constants.Cron_UpdateTagStats)] TimerInfo timer, FunctionContext context)
-//    {
-//        await RunAsync(timer, context);
-//    }
-//    protected override async Task<RunResult> RunAndCreateReportMailMainPartAsync(string defaultMailSubject)
-//    {
-//        var updater = new RefreshTagStats(NewCallContext());
-//        var result = await updater.RunAsync(new RefreshTagStats.Request());
+public sealed class UpdateTagStats : AbstractMemCheckAzureFunction
+{
+    public UpdateTagStats(TelemetryConfiguration telemetryConfiguration, MemCheckDbContext memCheckDbContext, MemCheckUserManager userManager, ILogger<UpdateTagStats> logger)
+        : base(telemetryConfiguration, memCheckDbContext, userManager, logger)
+    {
+    }
+    [Function(nameof(UpdateTagStats))]
+    public async Task Run([TimerTrigger(Constants.Cron_UpdateTagStats)] TimerInfo timer, FunctionContext context)
+    {
+        await RunAsync(timer, context);
+    }
+    protected override async Task<RunResult> RunAndCreateReportMailMainPartAsync(string defaultMailSubject)
+    {
+        var updater = new RefreshTagStats(NewCallContext());
+        var result = await updater.RunAsync(new RefreshTagStats.Request());
 
-//        var reportMailMainPart = new StringBuilder()
-//            .Append(CultureInfo.InvariantCulture, $"<h1>{result.Tags.Length} Mnesios tags</h1>")
-//            .Append("<p><table>")
-//            .Append("<thead><tr><th>Name</th><th>Previous count</th><th>Previous average</th><th>New count</th><th>New average</th></tr></thead>");
+        var reportMailMainPart = new StringBuilder()
+            .Append(CultureInfo.InvariantCulture, $"<h1>{result.Tags.Length} Mnesios tags</h1>")
+            .Append("<p><table>")
+            .Append("<thead><tr><th>Name</th><th>Previous count</th><th>Previous average</th><th>New count</th><th>New average</th></tr></thead>");
 
-//        foreach (var tag in result.Tags.OrderBy(tag => tag.TagName))
-//        {
-//            reportMailMainPart = reportMailMainPart
-//                .Append("<tr>")
-//                .Append(CultureInfo.InvariantCulture, $"<td>{tag.TagName}</td>");
-//            var cardCountChanged = tag.CardCountBeforeRun != tag.CardCountAfterRun;
-//            var averageChanged = tag.AverageRatingBeforeRun != tag.AverageRatingAfterRun;
-//            reportMailMainPart = reportMailMainPart.Append("<td>");
-//            if (cardCountChanged)
-//                reportMailMainPart = reportMailMainPart.Append("<strong>");
-//            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.CardCountBeforeRun}");
-//            if (cardCountChanged)
-//                reportMailMainPart = reportMailMainPart.Append("</strong>");
-//            reportMailMainPart = reportMailMainPart
-//                .Append("</td>")
-//                .Append("<td>");
-//            if (averageChanged)
-//                reportMailMainPart = reportMailMainPart.Append("<strong>");
-//            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.AverageRatingBeforeRun:0.##}");
-//            if (averageChanged)
-//                reportMailMainPart = reportMailMainPart.Append("</strong>");
-//            reportMailMainPart = reportMailMainPart
-//                .Append("</td>")
-//                .Append("<td>");
-//            if (cardCountChanged)
-//                reportMailMainPart = reportMailMainPart.Append("<strong>");
-//            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.CardCountAfterRun}");
-//            if (cardCountChanged)
-//                reportMailMainPart = reportMailMainPart.Append("</strong>");
-//            reportMailMainPart = reportMailMainPart
-//                .Append("</td>")
-//                .Append("<td>");
-//            if (averageChanged)
-//                reportMailMainPart = reportMailMainPart.Append("<strong>");
-//            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.AverageRatingAfterRun:0.##}");
-//            if (averageChanged)
-//                reportMailMainPart = reportMailMainPart.Append("</strong>");
-//            reportMailMainPart = reportMailMainPart
-//                .Append("</td>")
-//                .Append("</tr>");
-//        }
+        foreach (var tag in result.Tags.OrderBy(tag => tag.TagName))
+        {
+            reportMailMainPart = reportMailMainPart
+                .Append("<tr>")
+                .Append(CultureInfo.InvariantCulture, $"<td>{tag.TagName}</td>");
+            var cardCountChanged = tag.CardCountBeforeRun != tag.CardCountAfterRun;
+            var averageChanged = tag.AverageRatingBeforeRun != tag.AverageRatingAfterRun;
+            reportMailMainPart = reportMailMainPart.Append("<td>");
+            if (cardCountChanged)
+                reportMailMainPart = reportMailMainPart.Append("<strong>");
+            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.CardCountBeforeRun}");
+            if (cardCountChanged)
+                reportMailMainPart = reportMailMainPart.Append("</strong>");
+            reportMailMainPart = reportMailMainPart
+                .Append("</td>")
+                .Append("<td>");
+            if (averageChanged)
+                reportMailMainPart = reportMailMainPart.Append("<strong>");
+            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.AverageRatingBeforeRun:0.##}");
+            if (averageChanged)
+                reportMailMainPart = reportMailMainPart.Append("</strong>");
+            reportMailMainPart = reportMailMainPart
+                .Append("</td>")
+                .Append("<td>");
+            if (cardCountChanged)
+                reportMailMainPart = reportMailMainPart.Append("<strong>");
+            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.CardCountAfterRun}");
+            if (cardCountChanged)
+                reportMailMainPart = reportMailMainPart.Append("</strong>");
+            reportMailMainPart = reportMailMainPart
+                .Append("</td>")
+                .Append("<td>");
+            if (averageChanged)
+                reportMailMainPart = reportMailMainPart.Append("<strong>");
+            reportMailMainPart = reportMailMainPart.Append(CultureInfo.InvariantCulture, $"{tag.AverageRatingAfterRun:0.##}");
+            if (averageChanged)
+                reportMailMainPart = reportMailMainPart.Append("</strong>");
+            reportMailMainPart = reportMailMainPart
+                .Append("</td>")
+                .Append("</tr>");
+        }
 
-//        reportMailMainPart = reportMailMainPart.Append("</table></p>");
-//        return new RunResult(defaultMailSubject, reportMailMainPart);
-//    }
-//}
+        reportMailMainPart = reportMailMainPart.Append("</table></p>");
+        return new RunResult(defaultMailSubject, reportMailMainPart);
+    }
+}
