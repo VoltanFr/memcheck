38d4f4bd39d3396b2ab3284602e7a22b71eb6320
Vincent <mahonv@gmail.com>
2023-11-12 12:04:40 +0100

    New service in CommandLineDbClient: ListLanguages

---
+++ MemCheck.CommandLineDbClient/ManageDB/ListLanguages.cs
@@ -0,0 +1,32 @@
+﻿using MemCheck.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace MemCheck.CommandLineDbClient.ManageDB;
+
+internal sealed class ListLanguages : ICmdLinePlugin
+{
+    #region Fields
+    private readonly ILogger<ListUsers> logger;
+    private readonly MemCheckDbContext dbContext;
+    #endregion
+    public ListLanguages(IServiceProvider serviceProvider)
+    {
+        dbContext = serviceProvider.GetRequiredService<MemCheckDbContext>();
+        logger = serviceProvider.GetRequiredService<ILogger<ListUsers>>();
+    }
+    public void DescribeForOpportunityToCancel()
+    {
+        logger.LogInformation("Will list languages");
+    }
+    public async Task RunAsync()
+    {
+        var languages = await dbContext.CardLanguages.ToListAsync();
+
+        foreach (var language in languages)
+            logger.LogInformation($"Language '{language.Name}' has id {language.Id}");
+    }
+}
