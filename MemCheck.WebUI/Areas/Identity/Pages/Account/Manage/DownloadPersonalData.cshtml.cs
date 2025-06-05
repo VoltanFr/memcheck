fa49038e6392ebf03e542455e142bb3edf0ecbbe
Vincent <mahonv@gmail.com>
2025-06-05 08:50:44 +0200

    Minor improvement in DownloadPersonalData

--- MemCheck.WebUI/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ MemCheck.WebUI/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -37,7 +37,8 @@ public class DownloadPersonalDataModel : PageModel
             personalData.Add(p.Name, p.GetValue(user)?.ToString() ?? "null");
         }
 
-        Response.Headers.Add("Content-Disposition", "attachment; filename=PersonalData.json");
+        Response.Headers.ContentDisposition = "attachment; filename=PersonalData.json";
+
         return new FileContentResult(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(personalData)), "text/json");
     }
 }
