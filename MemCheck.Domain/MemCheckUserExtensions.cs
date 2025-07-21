c23f2db05237baa3a6576bfd686ef2b45faa5a6d
Vincent <mahonv@gmail.com>
2025-07-22 00:15:56 +0200

    Fixup: case of invalid email

--- MemCheck.Domain/MemCheckUserExtensions.cs
+++ MemCheck.Domain/MemCheckUserExtensions.cs
@@ -11,10 +11,6 @@ public static class MemCheckUserExtensions
     }
     public static MemCheckEmailAddress GetEmail(this MemCheckUser user)
     {
-        if (string.IsNullOrWhiteSpace(user.Email))
-            throw new InvalidProgramException("null user email");
-        if (string.IsNullOrWhiteSpace(user.UserName))
-            throw new InvalidProgramException("null user name");
-        return new MemCheckEmailAddress(user.Email, user.UserName);
+        return new MemCheckEmailAddress(user.Email ?? "", user.UserName ?? "");
     }
 }
